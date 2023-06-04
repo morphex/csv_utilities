cede0e8f7b29259802f4f304d3f8b60673959802
Morten W. Petersen <morphex@gmail.com>
2023-06-04 18:23:26 +0200

    Adding new test data, tests

--- mytests/mytest.sh
+++ mytests/mytest.sh
@@ -4,3 +4,9 @@ echo "First field sort"
 ./csv_sort testdata/shopping.csv
 echo "Fifth field sort"
 ./csv_sort testdata/shopping.csv 5
+echo "Fifth field sort, reverse"
+./csv_sort testdata/shopping.csv 5 -r
+echo "Fifth field sort, with kr in value, should fail"
+./csv_sort testdata/shopping-with-kr-text.csv 5
+echo "Fifth field sort, with kr in value, should work"
+./csv_sort testdata/shopping-with-kr-text.csv 5 -m
